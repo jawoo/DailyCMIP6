2ef8ad40f0e23e918f08ebddc7a976cedf317283
Jawoo Koo <j.koo@cgiar.org>
2023-05-21 20:13:59 -0400

    Initial commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,37 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class App
 {
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        // What to download
+        ArrayList<Object> urls = ListURLs.getURLs();
+
+        // Concurrency
+        try
+        {
+            ExecutorService executor = Executors.newFixedThreadPool(10);
+
+            // Download and upload
+            for (Object u: urls)
+            {
+                String fileUrl = (String)u;
+                String fileName = fileUrl.split("/")[fileUrl.split("/").length-1];
+                executor.submit(new DownloadFile(fileUrl, fileName));
+            }
+            executor.shutdown();
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
+
     }
+
 }
--- src/main/java/org/example/DownloadFile.java
+++ src/main/java/org/example/DownloadFile.java
@@ -1,2 +1,40 @@
-package org.example;public class DownloadFile {
+package org.example;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.concurrent.Callable;
+
+public class DownloadFile implements Callable<Object>
+{
+
+    String fileUrl;
+    String fileName;
+
+    DownloadFile(String fileUrl, String fileName)
+    {
+        this.fileUrl = fileUrl;
+        this.fileName = fileName;
+    }
+
+    @Override
+    public Object call()
+    {
+        try
+        {
+            InputStream inputStream = new URL(fileUrl).openStream();
+            InputStream in = new URL(fileUrl).openStream();
+            Files.copy(in, Paths.get("./out/"+fileName), StandardCopyOption.REPLACE_EXISTING);
+            System.out.println("> Downloaded: "+fileName);
+            inputStream.close();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
